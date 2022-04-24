488af30964acd41952f3dbe38c314758f280a075
rmpeng <60964708+rmpeng@users.noreply.github.com>
2022-04-24 13:18:31 +0800

    Add files via upload

--- TIE_EEGNet_en.py
+++ TIE_EEGNet_en.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
-from TIE_Layer_en import encoderLayer_ensemble
+from models.EnkLayer_en import EnkLayer_ensemble
 import torch
 import my_config
 GPU_ID = my_config.Config.GPU_id
@@ -13,7 +13,7 @@ class Conv2dWithConstraint(nn.Conv2d):
         self.weight.data = torch.renorm(self.weight.data, p=2, dim=0, maxnorm=self.max_norm)
         return super(Conv2dWithConstraint, self).forward(x)
 
-class TIE_EEGNet(nn.Module):
+class Enk_EEGNet(nn.Module):
     def CalculateOutSize(self, model, channels, samples):
         data = torch.rand(1,1,channels, samples)
         model.eval()
@@ -28,8 +28,8 @@ class TIE_EEGNet(nn.Module):
 
     def __init__(self, alpha_list, alpha=2, n_class = 4, channels = 20, samples = 512, dropoutRate = 0.5,
                  kernel_length = 64, kernel_length2 = 16, F1 = 8, F2 = 16, D = 2,
-                 encoder = 'sinusoidal', isTrain= True):
-        super(TIE_EEGNet, self).__init__()
+                 Enk = 'sinusodial', isTrain= True, pool='Avg'):
+        super(Enk_EEGNet, self).__init__()
         self.F1 = F1
         self.F2 = F2
         self.D = D
@@ -39,16 +39,17 @@ class TIE_EEGNet(nn.Module):
         self.dropoutRate = dropoutRate
         self.kernel_length = kernel_length
         self.kernel_length2 = kernel_length2
-        self.encoder = encoder
+        self.Enk = Enk
         self.isTrain = isTrain
         self.alpha_list=alpha_list
         self.alpha=alpha
+        self.pool = pool
 
 
         #self.Conv2d_1 = nn.Conv2d(1, self.F1, (1, self.kernel_length), padding='same', bias = False)#(padding =(1, self.kernel_size //2))
         self.Conv2d_1 = nn.Conv2d(1, self.F1, (1, self.kernel_length), padding=(0, self.kernel_length // 2), bias = False)#'same'
 
-        self.encoder_Layer = encoderLayer_ensemble(self.alpha_list, encoder= self.encoder, conv2Doutput= self.Conv2d_1, inc = 1, outc = self.F1,
+        self.Enk_Layer = EnkLayer_ensemble(pool= self.pool, alpha_list=self.alpha_list, Enk= self.Enk, conv2Doutput= self.Conv2d_1, inc = 1, outc = self.F1,
                                   kernel_size = (1, self.kernel_length), pad=(0, self.kernel_length // 2), stride = 1, bias = False,
                                   sample_len= self.samples,is_Train=self.isTrain,alpha=self.alpha)
         self.BatchNorm_1_1 = nn.BatchNorm2d(self.F1, momentum=0.01, affine=True, eps = 1e-3)
@@ -63,17 +64,17 @@ class TIE_EEGNet(nn.Module):
         self.avg_pool_2 = nn.AvgPool2d((1, 8), stride= 8)
         self.Dropout_2 = nn.Dropout(p= self.dropoutRate)
 
-        self.fea_model = nn.Sequential(self.encoder_Layer,
+        self.fea_model = nn.Sequential(self.Enk_Layer,
                                        self.BatchNorm_1_1,
                                        self.Depthwise_Conv2d,
                                        self.BatchNorm_1_2,
-                                       nn.ELU(),
+                                       nn.ELU(inplace=True),
                                        self.avg_pool_1,
                                        self.Dropout_1,
                                        self.Separable_Conv2d_1,
                                        self.Separable_Conv2d_2,
                                        self.BatchNorm_2,
-                                       nn.ELU(),
+                                       nn.ELU(inplace=True),
                                        self.avg_pool_2,
                                        self.Dropout_2)
 
--- TIE_Layer_en.py
+++ TIE_Layer_en.py
@@ -7,25 +7,35 @@ GPU_ID = my_config.Config.GPU_id
 device = torch.device('cuda:{}'.format(GPU_ID) if torch.cuda.is_available() else "cpu")
 
 
-class encoderLayer_ensemble(nn.Module):
-    def __init__(self, alpha_list, encoder, conv2Doutput, inc, outc, kernel_size, pad, stride, bias, sample_len, is_Train=True, alpha=2):
-        super(encoderLayer_ensemble, self).__init__()
+class EnkLayer_ensemble(nn.Module):
+    def __init__(self, pool,alpha_list, Enk, conv2Doutput, inc, outc, kernel_size, pad, stride, bias, sample_len, is_Train=True, alpha=2):
+        super(EnkLayer_ensemble, self).__init__()
         #alpha_list #[2,4,8,10,16,32,256]
         self.list_alpha = list(alpha_list)
-        self.encoder = encoder
+        self.Enk = Enk
         self.isTrain = is_Train
         self.sample_len = sample_len
         self.alpha = alpha
+        self.pool=pool
         self.new_alpha = self.alpha
         self.conv2DOutput = conv2Doutput
-        self.auxiliary_conv = nn.Conv2d(in_channels=inc, out_channels=outc, kernel_size=kernel_size, padding=pad, stride=stride,
-                                        bias=bias)
-        self.weight_mat = np.ones(shape=[outc, inc, self.auxiliary_conv.kernel_size[0], self.auxiliary_conv.kernel_size[1]])
+        if self.pool=='Avg':
+            self.pooling = nn.AvgPool2d(kernel_size=kernel_size, stride=stride, padding=pad)
+        else:
+            self.pooling = nn.MaxPool2d(kernel_size=kernel_size, stride=stride, padding=pad)
+
+        # self.auxiliary_conv = nn.Conv2d(in_channels=inc, out_channels=outc, kernel_size=kernel_size, padding=pad, stride=stride,
+        #                                 bias=bias)  # 辅助卷积
+        # self.weight_mat = np.ones(shape=[outc, inc, self.auxiliary_conv.kernel_size[0], self.auxiliary_conv.kernel_size[1]])
         self.fix_encodings_base = torch.range(0,  sample_len , 1, dtype=torch.float32)  # .to(device)
 
-        self.auxiliary_conv.weight = nn.Parameter(torch.Tensor(self.weight_mat), requires_grad=False)
+        #self.auxiliary_conv.weight = nn.Parameter(torch.Tensor(self.weight_mat), requires_grad=False)
+        if self.Enk == 'linear':
+            self.scale_factor = nn.Parameter(torch.zeros(1, ), requires_grad=True)
+            self.scale_factor = self.scale_factor  # .to(device)
+            self.scale_factor.requires_grad_ = True
 
-        if self.encoder == 'sinusoidal':
+        elif self.Enk == 'sinusodial':
             self.scale_factor = nn.Parameter(torch.zeros(1, ), requires_grad=True)  # .to(device)
             self.scale_factor.requires_grad_ = True
 
@@ -42,7 +52,7 @@ class encoderLayer_ensemble(nn.Module):
         self.new_alpha = random.sample(self.list_alpha,1)[0]
         self.fix_encodings_base = torch.range(0,  self.sample_len , 1, dtype=torch.float32)  # .to(device)
 
-        if self.encoder == 'sinusoidal':
+        if self.Enk == 'sinusodial':
             for i in range(self.sample_len):
                 if i % 2 == 0:
                     self.fix_encodings_base[i] = torch.sin(self.fix_encodings_base[i] / self.new_alpha)
@@ -58,7 +68,7 @@ class encoderLayer_ensemble(nn.Module):
         else:
             self.new_alpha = self.alpha
             self.fix_encodings_base = torch.range(0, self.sample_len, 1, dtype=torch.float32)  # .to(device)
-            if self.encoder == 'sinusoidal':
+            if self.Enk == 'sinusodial':
                 for i in range(self.sample_len):
                     if i % 2 == 0:
                         self.fix_encodings_base[i] = torch.sin(self.fix_encodings_base[i] / self.new_alpha)
@@ -67,12 +77,12 @@ class encoderLayer_ensemble(nn.Module):
 
         try:
             newconv = torch.add(self.conv2DOutput(data),
-                               0.1* torch.mul(self.auxiliary_conv(data), torch.reshape(self.fix_encodings_base * self.scale_factor, [1, 1, 1, -1])))
+                               torch.mul(self.pooling(data), torch.reshape(self.fix_encodings_base * self.scale_factor, [1, 1, 1, -1])))
             # self.fix_encodings_base=self.fix_encodings_base.to(device)
         except:
             self.fix_encodings_base = self.fix_encodings_base.to(device)
             torch.reshape(self.fix_encodings_base * self.scale_factor, [1, 1, 1, -1]).to(device)
             newconv = torch.add(self.conv2DOutput(data),
-                                0.1 * torch.mul(self.auxiliary_conv(data),
+                                torch.mul(self.pooling(data),
                                           torch.reshape(self.fix_encodings_base * self.scale_factor, [1, 1, 1, -1])))
         return newconv
